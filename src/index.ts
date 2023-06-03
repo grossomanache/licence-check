ccb575e7c4f9673abf1834b900c08a1506c18124
Elías Heresi <elias.heresi@gmail.com>
2023-06-03 17:35:01 -0400

    Modularized first screen navigation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import inputTextOnField from "./inputting";
 import delayExecution from "./utils/delayExecution";
+import addBasicInformation from "./navigation/addBasicInformation";
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -11,43 +12,10 @@ import delayExecution from "./utils/delayExecution";
       "http://appl.smc.cl/lc/SolicitaHoras/muni/vitacura.aspx";
     await page.goto(vitacuraUrl);
 
-    const rutId = "#ctl00_ContentPlaceHolder1_txtRut";
     const rut = "189325673";
-
-    await inputTextOnField(page, "input", rut, { combinator: rutId });
-
-    const birthDateId = "#ctl00_ContentPlaceHolder1_txtFechaNac";
     const birthDate = "14081994";
-    const birthDateSelector = `input${birthDateId}`;
 
-    await page.click(birthDateSelector);
-
-    await inputTextOnField(page, "input", birthDate, {
-      combinator: birthDateId,
-      delay: 700,
-    });
-
-    const submitButtonId = "ctl00_ContentPlaceHolder1_btnAceptar";
-    const submitButtonSelector = `input#${submitButtonId}`;
-
-    await page.click(submitButtonSelector);
-
-    const delay = delayExecution(1000);
-    await delay;
-
-    const alertId = "ctl00_ContentPlaceHolder1_LblMsj";
-    const alertSelector = `span#${alertId}`;
-    const alert = await page.waitForSelector(alertSelector);
-
-    if (alert) {
-      const errorMessage = await (
-        await alert.getProperty("textContent")
-      ).jsonValue();
-
-      throw new Error(errorMessage ?? "Error");
-    }
-
-    debugger;
+    addBasicInformation(page, rut, birthDate);
   } catch (error) {
     console.log(error);
     browser.close();
---
+++ src/navigation/addBasicInformation.ts
@@ -0,0 +1,43 @@
+import { Page } from "puppeteer";
+import inputTextOnField from "../inputting";
+import delayExecution from "../utils/delayExecution";
+
+const addBasicInformation = async (
+  page: Page,
+  rut: string,
+  birthDate: string
+) => {
+  const rutId = "#ctl00_ContentPlaceHolder1_txtRut";
+  await inputTextOnField(page, "input", rut, { combinator: rutId });
+
+  const birthDateId = "#ctl00_ContentPlaceHolder1_txtFechaNac";
+  const birthDateSelector = `input${birthDateId}`;
+
+  await page.click(birthDateSelector);
+
+  await inputTextOnField(page, "input", birthDate, {
+    combinator: birthDateId,
+    delay: 700,
+  });
+
+  const submitButtonId = "ctl00_ContentPlaceHolder1_btnAceptar";
+  const submitButtonSelector = `input#${submitButtonId}`;
+
+  await page.click(submitButtonSelector);
+
+  await delayExecution(1000);
+
+  const alertId = "ctl00_ContentPlaceHolder1_LblMsj";
+  const alertSelector = `span#${alertId}`;
+  const alert = await page.waitForSelector(alertSelector);
+
+  if (alert) {
+    const errorMessage = await (
+      await alert.getProperty("textContent")
+    ).jsonValue();
+
+    throw new Error(errorMessage ?? "Error");
+  }
+};
+
+export default addBasicInformation;
